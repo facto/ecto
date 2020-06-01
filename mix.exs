368ca7a8a4114e489d0cd43fd5c6e967a99c3a13
Joshua Rieken <joshua@joshuarieken.com>
2020-06-01 17:02:20 -0600

    Remove postgrex reference

--- mix.exs
+++ mix.exs
@@ -44,7 +44,7 @@ defmodule Ecto.Mixfile do
 
      # Drivers
      {:db_connection, "~> 1.1", optional: true},
-     {:postgrex, "~> 0.13.0", optional: true},
+     # {:postgrex, "~> 0.13.0", optional: true},
      {:mariaex, "~> 0.8.0", optional: true},
 
      # Optional
